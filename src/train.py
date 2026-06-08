93be0a99f562ff87d34902861babc3f40c651668
Anurag Maurya <justanuragmaurya@gmail.com>
2026-06-08 23:09:01 +0530

    made a simple bigram model

--- src/train.py
+++ src/train.py
@@ -1,9 +1,11 @@
 import torch
 from data_tokenize import DataTokenizer
 from config import CONTEXT_SIZE , BATCH_SIZE
+from model import BiGramModel
 
 tokenizer = DataTokenizer("data.txt")
 tokens = tokenizer.tokenize()
+vocab_size = tokenizer.vocab_size
 
 split = int(0.9*len(tokens))
 train_data = tokens[:split]
@@ -16,10 +18,21 @@ def get_batches(mode):
     y = torch.stack([data[i+1:i+CONTEXT_SIZE+1]for i in ix])
     return x,y
 
-xb,yb = get_batches("TRAIN")
+model = BiGramModel(vocab_size)
+optim = torch.optim.AdamW(model.parameters(),lr=1e-3)
 
-print(xb.shape)
-print(yb.shape)
+# xb,yb = get_batches("TRAIN")
+print("Before Training \n")
+print(tokenizer.decode(model.generate(torch.zeros((1,1),dtype=torch.long),max_new_token=400)[0].tolist()))
 
-print(xb)
-print(yb)
+for steps in range(10000):
+    xb , yb = get_batches("TRAIN")
+    logits , loss = model(xb,yb)
+    optim.zero_grad(set_to_none=True)
+    loss.backward()
+    optim.step()
+    if(steps%100==0):
+        print(loss)
+
+print("After Training \n")
+print(tokenizer.decode(model.generate(torch.zeros((1,1),dtype=torch.long),max_new_token=400)[0].tolist()))
