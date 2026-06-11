fb4d52f16d76f840fdd1411557ee6c77e600e6d3
Anurag Maurya <justanuragmaurya@gmail.com>
2026-06-12 00:27:10 +0530

    context using weighted average and pos info

--- src/model.py
+++ src/model.py
@@ -1,18 +1,19 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-from config import N_EMBED , CONTEXT_SIZE
+from config import N_EMBED , CONTEXT_SIZE , BATCH_SIZE
 
 class BiGramModel(nn.Module):
     def __init__(self,vocab_size):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size,N_EMBED)
-        self.pos_embedding_table = nn.Embedding(block_size,N_EMBED)
+        self.pos_embedding_table = nn.Embedding(CONTEXT_SIZE,N_EMBED)
         self.lm_head = nn.Linear(N_EMBED,vocab_size)
     
     def forward(self,idx,targets=None):
+        B,T = idx.shape
         tok_embedings  = self.token_embedding_table(idx)
-        pos_embedding = self.pos_embedding_table(torch.arange(CONTEXT_SIZE))
+        pos_embedding = self.pos_embedding_table(torch.arange(T))
         x = tok_embedings+pos_embedding
         logits = self.lm_head(x)
         
@@ -26,11 +27,12 @@ class BiGramModel(nn.Module):
         
         return logits,loss
     
-    def generate(self,idx,max_new_token):
+    def generate(self, idx, max_new_token):
         for _ in range(max_new_token):
-            logits , loss = self(idx)
-            logits = logits[:,-1,:]
-            probs = F.softmax(logits,dim=-1)
-            idx_next = torch.multinomial(probs,num_samples=1)
-            idx = torch.cat((idx,idx_next),dim=1)
+            idx_cond = idx[:, -CONTEXT_SIZE:]
+            logits, loss = self(idx_cond)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
         return idx
