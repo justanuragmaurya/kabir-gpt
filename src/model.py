b4a8a6fe31242e115e4d4ce06fd5d35363740dd2
Anurag Maurya <justanuragmaurya@gmail.com>
2026-06-12 23:15:51 +0530

    implemented the full gpt-style decoder only transformer model

--- src/model.py
+++ src/model.py
@@ -30,17 +30,54 @@ class MultiHeadAttention(nn.Module):
     def __init__(self,num_head,head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_head)])
+        self.projection = nn.Linear(N_EMBED,N_EMBED)
 
     def forward(self,x):
-        return torch.cat([h(x) for h in self.heads],dim=-1)
+        out = torch.cat([h(x) for h in self.heads],dim=-1)
+        out = self.projection(out)
+        return out
+
+
+class FeedFwd(nn.Module):
+    def __init__(self,n_embed):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embed,n_embed),
+            nn.ReLU(),
+            nn.Linear(n_embed,n_embed),
+        )
+    
+    def forward(self,x):
+        return self.net(x)
+
+class Block(nn.Module):
+    def __init__(self,n_embed,n_head):
+        super().__init__()
+        head_size = n_embed//n_head
+        self.self_attn = MultiHeadAttention(n_head,head_size)
+        self.feed_fwd = FeedFwd(n_embed)
+        self.ln1 = nn.LayerNorm(n_embed)
+        self.ln2 = nn.LayerNorm(n_embed)
+
+    def forward(self,x):
+        x = x + self.self_attn(self.ln1(x))
+        x = x + self.feed_fwd(self.ln2(x))
+        return x
 
 class BiGramModel(nn.Module):
     def __init__(self,vocab_size):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size,N_EMBED)
         self.pos_embedding_table = nn.Embedding(CONTEXT_SIZE,N_EMBED)
-        # self.attn_head = Head(N_EMBED)
-        self.attn_head = MultiHeadAttention(4,N_EMBED//4)
+        # # self.attn_head = Head(N_EMBED)
+        # self.attn_head = MultiHeadAttention(4,N_EMBED//4)
+        # self.feed_fwd = FeedFwd(N_EMBED)
+        self.blocks = nn.Sequential(
+            Block(N_EMBED,n_head=4),
+            Block(N_EMBED,n_head=4),
+            Block(N_EMBED,n_head=4),
+            nn.LayerNorm(N_EMBED)
+        )
         self.lm_head = nn.Linear(N_EMBED,vocab_size)
     
     def forward(self,idx,targets=None):
@@ -48,7 +85,9 @@ class BiGramModel(nn.Module):
         tok_embedings  = self.token_embedding_table(idx)
         pos_embedding = self.pos_embedding_table(torch.arange(T))
         x = tok_embedings+pos_embedding
-        x = self.attn_head(x)
+        # x = self.attn_head(x)
+        # x = self.feed_fwd(x)
+        x = self.blocks(x)
         logits = self.lm_head(x)
         
         if targets == None:
